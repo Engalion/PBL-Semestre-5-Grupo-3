90cb45a7c47390eb0c79954c9471627a7e6a0b13
Alejandro Agrela <144915117+Engalion@users.noreply.github.com>
2025-12-16 12:56:37 +0000

    Update db.php

--- db.php
+++ db.php
@@ -4,12 +4,14 @@ $user = 'root';
 $pass = ''; 
 $db   = 'datacenter';
 
+// Cria a conexão com o servidor MySQL
 $conn = new mysqli($host, $user, $pass, $db);
 
+// Verifica se ocorreu erro ao conectar
 if ($conn->connect_error) {
     die("Erro na ligação à BD: " . $conn->connect_error);
 }
 
-// opcional: definir charset
+// Define o charset da conexão (evita problemas com acentos)
 $conn->set_charset("utf8");
 ?>
